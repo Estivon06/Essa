5b89ad2c50f64f581c0ce254aaf8baf03f5ffec1
Estivon06 <blondono2@udi.edu.co>
2025-10-08 20:19:31 -0500

    first commit

---
+++ ServiciosElectricos/core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def contacto(request):
+    return render(request, 'contacto.html')
